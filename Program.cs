35ad560089d341efcd99645aabf0b04af94ff630
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-19 17:15:13 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class MyClass
+{
+    public static string[] tasks = new string[] { "task1", "task2", "task3", "task4", "task5", "task6" };
+    public static Queue<string> queue = new Queue<string>(tasks);
+    public static void Main(String[] args)
+    {
+        
+        Display();
+    }
+   
+   
+
+    public static void Display()
+    {
+        Console.WriteLine("Members in queue are: ");
+        foreach (string i in queue)
+        {
+            Console.Write(i + ", ");
+        }
+        Console.WriteLine("Total number of elements are " + queue.Count);
+        if (queue.Count > 0)
+        {
+            Console.WriteLine("First Element of the queue is " + queue.Peek());
+        }
+
+
+    }
+}

59cee1b40ead0efbb16e005e115d04963f2e8984
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-19 17:16:08 +0530

    Features using queue are added

--- Program.cs
+++ Program.cs
@@ -6,9 +6,69 @@
     {
         
         Display();
+        QueueFetures();
     }
    
-   
+    private static void QueueFetures()
+    {
+        while (true)
+        {
+            
+            Console.WriteLine("Please enter choise from following. ");
+            Console.WriteLine("1 To Enqueue a string into a queue");
+            Console.WriteLine("2 To Dequeue a string from a queue");
+            Console.WriteLine("3 To check if perticular string is in queue");
+            Console.WriteLine("4 To clear the queue");
+            Console.WriteLine("5 To quit");
+
+            int choise = -1;
+            try
+            {
+                choise = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("Please enter a string to Enqueue");
+                    string str = Console.ReadLine();
+
+                    MyClass.queue.Enqueue(str);
+                    Display();
+                    break;
+                case 2:
+                    str = queue.Dequeue();
+                    Console.WriteLine(str + " Dequeued.");
+                    Display();
+                    break;
+                case 3:
+                    Console.WriteLine("Please enter a string to check if it's in queue");
+                    str = Console.ReadLine();
+                   
+                    if (queue.Contains(str))
+                    {
+                        Console.WriteLine("Yes " + str + " is in queue");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No " + str + " is not in queue");
+                    }
+                    Display();
+                    break;
+                case 4:
+                    queue.Clear();
+                    Display();
+                    break;
+                default:
+                    Console.WriteLine("Exiting");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
 
     public static void Display()
     {
